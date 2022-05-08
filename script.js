b1a4dcf3da50d9aaa848a17f38525a1f6cfe2b24
shreyas patil <shreyaspatil1901@gmail.com>
2022-05-08 15:25:05 +0530

    modification in code

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
+var color2 = document.querySelector(".color2"); 
 var body = document.getElementById("gradient");
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+
+function setgradient(){
+	body.style.background = "Linear-gradient(to right," + 
+	color1.value + ", " + color2.value +")";
 
 	css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
+// color1.addEventListener("input", setgradient);
 
-color2.addEventListener("input", setGradient);
+// color2.addEventListener("input", setgradient);
+
